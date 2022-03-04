c7b193735a4a1894a53fb6e71e9afa5523d4be4c
Royce Arockiasamy <royceaden@gmail.com>
2022-03-03 20:20:49 -0500

    added march meeting

--- assets/js/authorized.js
+++ assets/js/authorized.js
@@ -122,14 +122,13 @@ function data_setup(obj) {
         ${obj.attendance.past_attendance[i].completed ? "<b>COMPLETED</b>": "<b>INCOMPLETE</b>"} attendance for the ${obj.attendance.past_attendance[i].type} meeting. View the <a target="_blank"  href="${obj.attendance.past_attendance[i].video_url}"> Meeting Video</a>. View the <a target="_blank"  href="${obj.attendance.past_attendance[i].slideshow_url}"> Meeting Slideshow Presentation</a>. 
         `;
        document.getElementById("p_attendance").appendChild(main_div);
-       /*if(obj.attendance.current_attendance[i]) {
-        if(obj.attendance.current_attendance[i].type == "February" && !obj.attendance.current_attendance[i].completed) {
+       if(obj.attendance.current_attendance[i]) {
+        if(obj.attendance.current_attendance[i].type == "March" && !obj.attendance.current_attendance[i].completed) {
             document.getElementById("monthly_video").style.display = "block";
         } else {
             document.getElementById("completed_video").style.display = "block";
         }
        }
-      */
         
     }
     for(let i=0; i<obj.attendance.current_attendance.length; i++) {
@@ -924,7 +923,7 @@ function completedAttendance(correct) {
         let dataABC = {
             "present": true,
             "userID": email,
-            "init": 5
+            "init": 6
         }
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
@@ -940,7 +939,7 @@ function completedAttendance(correct) {
         let dataABC = {
             "present": false,
             "userID": email,
-            "init": 5
+            "init": 6
         }
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function() {
