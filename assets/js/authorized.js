db054b43934e8eb72271271f8a5fed4d461a7bd3
Royce Arockiasamy <royceaden@gmail.com>
2023-01-11 17:38:38 -0500

    Update authorized.js

--- assets/js/authorized.js
+++ assets/js/authorized.js
@@ -7,6 +7,7 @@ var admin = param.get("admin") || getCookie("admin");
 const error = param.get("error") || null;
 var total = 0;
 var mainObj;
+let productLink = "https://sfhsbetabackend.roycea.repl.co";
 
 if(error == undefined) {
     if(admin != undefined || getCookie("admin") != undefined) {
@@ -628,7 +629,7 @@ function updateVOP() {
             window.location = "dashboard.html";
         } 
     };
-    xhttp.open("POST", `${productionLink}/updateVOP`, true);
+    xhttp.open("POST", `${productLink}/updateVOP`, true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send(JSON.stringify(dataABC));
 }
@@ -645,7 +646,7 @@ function deleteVOP() {
             window.location = "dashboard.html";
         } 
     };
-    xhttp.open("POST", `${productionLink}/deleteVOP`, true);
+    xhttp.open("POST", `${productLink}/deleteVOP`, true);
     xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
     xhttp.send(JSON.stringify(dataABC));
 }
@@ -767,7 +768,7 @@ function closeCreateVolunteeringOpportunity() {
 
 
  function validateVolunteeringOpportunity() {
-    document.getElementById("volOPS").action = `${productionLink}addVolunteeringOpportunity`;
+    document.getElementById("volOPS").action = `${productLink}addVolunteeringOpportunity`;
     let upload_image = document.getElementById("document_file").files.length;
     let title = document.getElementById("title_volunteeringOP").value;
     let description = document.getElementById("description_volunteeringOP").innerHTML;
