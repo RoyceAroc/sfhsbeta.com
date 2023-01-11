35c511eb3d4c7948fafb5ea42c25e327bb409ab0
Royce Arockiasamy <royceaden@gmail.com>
2023-01-11 17:13:55 -0500

    Update authorized.js

--- assets/js/authorized.js
+++ assets/js/authorized.js
@@ -767,7 +767,7 @@ function closeCreateVolunteeringOpportunity() {
 
 
  function validateVolunteeringOpportunity() {
-    document.getElementById("volOPS").action = `${productionLink}/addVolunteeringOpportunity`;
+    document.getElementById("volOPS").action = `${productionLink}addVolunteeringOpportunity`;
     let upload_image = document.getElementById("document_file").files.length;
     let title = document.getElementById("title_volunteeringOP").value;
     let description = document.getElementById("description_volunteeringOP").innerHTML;
